46e29da8ba86a92ba36a97bf19727be63a7560fa
skiernan <33738013+skiernan@users.noreply.github.com>
2019-09-29 05:11:01 -0400

    Update spell.c

--- spell.c
+++ spell.c
@@ -1,1 +1,3 @@
-
+int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]);
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]);
+bool check_word(const char* word, hashmap_t hashtable[]);
